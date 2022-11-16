5d411b63f2594d2ab7d751e370f342d8da6efb4f
Romeo <53314439+Ro-off@users.noreply.github.com>
2022-11-16 20:40:19 +0200

    Update TutorialModClient.java

--- src/main/java/net/halfheart/tutorialmod/TutorialModClient.java
+++ src/main/java/net/halfheart/tutorialmod/TutorialModClient.java
@@ -7,6 +7,7 @@ public class TutorialModClient implements ClientModInitializer{
     public void onInitializeClient() {
         // This code runs on the client only.
         // You can use this to add things like custom textures, models, and so on.
+        
     }
     
 }
