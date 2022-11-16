cc222b957ade98655f06f667ee7a52d5aa9c7e6f
Romeo <53314439+Ro-off@users.noreply.github.com>
2022-11-16 20:38:58 +0200

    Mod Creation

---
+++ src/main/java/net/halfheart/tutorialmod/TutorialModClient.java
@@ -0,0 +1,12 @@
+package net.halfheart.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+        // This code runs on the client only.
+        // You can use this to add things like custom textures, models, and so on.
+    }
+    
+}
